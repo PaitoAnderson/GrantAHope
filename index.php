5dc2ee3e892e3f55ec167c9e0b48fa474b97407e
Paito Anderson <pj.paito@gmail.com>
2015-06-21 20:27:19 -0400

    Initial Code Import

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="robots" content="index,follow" />
+	<meta name="description" content="Grantahope.com is the personal website of Grant Hope a IT student from Windsor, ON." />
+	<meta name="keywords" content="grantahope.com, Grant Hope, Grant, Hope, Blacks Photography, Telus, cisco, ccna, student, St. Clair College, Belle River District High School, windsor on" />
+	<meta name="geo.region" content="CA-ON" />
+	<meta name="geo.placename" content="Windsor" />
+	<meta name="geo.position" content="42.314111;-83.036825" />
+	<meta name="ICBM" content="42.314111, -83.036825" />
+	<link rel="icon" type="image/png" href="favicon.ico" />
+	<link rel="stylesheet" href="screen.css" type="text/css" media="screen, projection, tv"/>
+	<link rel="stylesheet" href="print.css" type="text/css" media="print"/>
+	<script type='text/javascript' src='tipsy/jquery-1.3.2.min.js'></script> 
+	<link rel="stylesheet" href="tipsy/tipsy.css" type="text/css" /> 
+	<script type="text/javascript" src="tipsy/jquery.tipsy.js"></script>
+	<script type="text/javascript">
+	<!--
+		function stringReverse(textString) {
+		   if (!textString) return '';
+		   var revString='';
+		   for (i = textString.length-1; i>=0; i--)
+		       revString+=textString.charAt(i)
+		   return revString;
+		}
+		function showEM(userName,emServer) {
+			userName = stringReverse(userName);
+			emServer = stringReverse(emServer);
+			var emLink = userName+"@"+emServer;
+			document.write("<a class='plain' href='mailto:"+emLink+"'>");
+			document.write(emLink);
+			document.write("</a>");
+		}
+	-->
+	</script>
+	<title>Grant Hope</title>
+	<script type='text/javascript'> 
+	  $(function() {
+	    $('#facebook').tipsy({gravity: 'n'});
+	    $('#linkedin').tipsy({gravity: 'n'});
+	    $('#google').tipsy({gravity: 'n'}); /*{fade: true}*/
+	  });
+	</script>
+</head>
+<body>
+	<div id="container"> 
+		
+		<!--<div id="save"> 
+			<a href="javascript:window.print()">Print</a>|<a href="#">Download PDF</a>|<a href="#">Email Me</a> 
+		</div>--> 
+		<div id="content">
+			<div id="header"> 
+				<div id="info"> 
+					<div id="name"> 
+						<h1>Grant Hope</h1> 
+					</div> 
+					<div id="wep"> 
+						<h2><span class="blue"><span class="website">w</span>:</span> <a class="plain" href="http://grantahope.com/">grantahope.com</a></h2> 
+						<h2><span class="blue"><span class="email">e</span>:</span> <script type="text/javascript">showEM("epohatnarg","moc.liamg");</script></h2> 
+						<h2 class="listend"><span class="blue"><span class="phone">p</span>:</span> <a class="plain" href="tel:+1519-560-7078">+1 (519) 560-7078</a></h2> 
+					</div>
+				</div>
+
+				<div id="photo"> 
+					<img src="me.jpg" alt="Grant Hope" title="Grant Hope" />
+				</div>
+				<div id="social">
+						<a target="_blank" href="https://www.facebook.com/grant.hope" id="facebook" title="Facebook Profile" class="tip"><img src="social/facebook.png" alt="facebook icon" /></a>
+						<a target="_blank" href="https://www.linkedin.com/profile/view?id=118536300" id="linkedin" title="Linkedin Profile" class="tip"><img src="social/linkedin.png"  alt="linkedin icon"/></a>
+						<a target="_blank" href="https://plus.google.com/+GrantHope/" id="google" title="Google Plus" class="tip"><img src="social/google.png"  alt="google icon"/></a>
+				</div>
+			</div>
+			<div id="lcolumn">
+				<h4>Job Objective</h4><br />
+				<div class="notes">To become a valued member of an Information Technology support team, in the role of Systems Administrator, and to one day seek a management position in this field.</div>
+				<div class="division"></div>
+				
+				<h4>Education</h4><br />
+				<div class="title">St. Clair College</div>
+				<div class="date">2008 – 2011</div><!-- 2008 - April 2011-->
+				<div class="notes">Computer Systems Technologist – GPA 3.626</div>
+				<div class="division"></div>
+				<div class="title">Belle River District High School</div>
+				<div class="date">2004 - 2008</div><!--Sept 2004 - 2008-->
+				<div class="notes">High School Diploma, Honour Roll.</div>
+				<div class="division"></div>
+				
+				<h4>Work Experience</h4><br />
+				<div class="title">Compugen </div>
+				<div class="date">2011 - present</div>
+				<div class="notes bullet">
+					<ul>
+						<li>Operation Support Analyst</li>
+						<li>Develop Processes</li>
+						<li>Test and maintain software and documentation in the environment</li>
+						<li>Distribute software and Administer Microsoft’s SCCM</li>
+						<li>Create install and runtime scripts with Autoit</li>
+						<li>Developed Windows 7 and Windows XP production images</li>
+						<li>Prioritise issues as they arise and correct them</li>
+					</ul>
+				</div>
+				<div class="title">Blacks Photography</div>
+				<div class="date">2007 - 2011</div>
+				<div class="notes bullet">
+					<ul>
+						<li>Part-time sales associate</li>
+						<li>Extensive knowledge with compact cameras and SLR's (digital and film)</li>
+						<li>Lab training – Processing and development 35mm film and processing digital photo files</li>
+						<li>Maintaining Fuji chemical printer and film C-41 and dry lab equipment</li>
+						<li>WHIMIS training</li>
+						<li>Chemical spill response training</li>
+                        <li>TELUS wireless training</li>
+					</ul>
+				</div>
+				<div class="division"></div>
+			</div>
+			<div id="rcolumn"> 
+				<h4>Volunteer Experience</h4><br />
+                <div class="title">Network Lab Assistant</div>
+                <div class="date">2010 - 2011</div><!--2011 Sept - April-->
+                <div class="notes">Collaborated and developed various security labs</div>
+                <div class="notes">Assisted various instructors with the delivery of hands on lab assignments throughout the year</div>
+				<div class="division"></div>
+				<div class="title">The FireFest Combat Challenge</div>
+				<div class="date">2005 - 2007</div>
+				<div class="notes">Duties included acting as Security at the main entrance to ensure no unapproved entry was made by pedestrians and vehicles</div>
+                <div class="notes">Assisted with the set up and dismantling of the event.</div>
+				<div class="division"></div>
+				
+				<h4>Technical Expertise</h4><br />
+				<div class="notes bullet">
+					<ul>
+						<li>First place St Clair College networking competition and participated at the Ontario Skills Competition for Computer Networking (2011)</li>
+						<li>Designed a Network Specification Document which encompasses Requirements Gathering phase, Traffic Analysis phase, Logical design, Physical design.</li>
+						<li>Extensive experience with Microsoft products, both current and legacy OS </li>
+						<li>Designing, installing, and configuring Local Area Networks and designing and simulating Wide Area Networks in a lab environment</li>
+						<li>Knowledge and experience in lab using TCP/IP; and Routing Protocols: RIP1/2, EIGRP, and OSPF.</li>
+                        <li>Extensive troubleshooting experience using Windows Administrative Tools in Server 2003/2008.</li>
+                        <li>Experienced in configuring Certificate Services, Remote Access, IPSec, and VPN's</li>
+					</ul>
+				</div>
+				
+				<h4>Certifications</h4><br />
+                <div class="notes">Cisco Certified Networking Associate (CCNA) certification as of September of 2010 </div>
+                <div class="division"></div>
+				<div class="notes print"><i>References available upon request.</i></div>
+				
+				<div class="division"></div>
+				<div class="notes reference"><i>References available upon request.</i></div>
+			</div>
+		</div>
+		<div id="footer">
+		</div>
+		<br /><br />
+	</div>
+	<script type="text/javascript">
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-24932294-2', 'auto');
+	  ga('send', 'pageview');
+	</script> 
+</body>
+</html>
